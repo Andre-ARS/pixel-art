098fca527282a1a90e5cea0b97f19e5447cb856f
André Rodrigues <andre360.ars@gmail.com>
2021-12-09 19:16:09 -0300

    Requisito 5 - da estilo ao grid de pixels

---
+++ script.js
@@ -0,0 +1,18 @@
+// let pixels = document.querySelector('#grid-length')
+// let grid = document.querySelector('#pixel-board')
+
+// function createBoard(base) {
+//     for (let index = 0; index < base; index += 1) {
+//         let line = document.createElement('section')
+
+//         grid.appendChild(line)
+//         for (let i = 0; i < base; i += 1) {
+//             let pixel = document.createElement('div')
+
+//             pixel.className = 'pixel'
+//             line.appendChild(pixel)
+//         }       
+//     }
+// }
+
+// pixels.addEventListener('change', createBoard(document.querySelector('#grid-length').value))
