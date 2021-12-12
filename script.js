73ab21abb945750c7e07807aaea4bec8fdbb38c7
André Rodrigues <andre360.ars@gmail.com>
2021-12-12 17:05:43 -0300

    Requisito 10 - bonus - Cria input e botão para deixar a criação da board dinamica

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-let gridLength = document.querySelector('#grid-length')
 let grid = document.querySelector('#pixel-board')
 let colorPallet = document.querySelectorAll('#color-palette div')
 let pickedColor = document.getElementsByClassName('color selected')[0]
 let clearBoard = document.getElementById('clear-board')
 let pixels = document.getElementsByClassName('pixel')
+let boardSize = document.getElementById('board-size')
 
-// function createBoard(base) {
-//     for (let index = 0; index < base; index += 1) {
-//         let line = document.createElement('section')
 
-//         grid.appendChild(line)
-//         for (let i = 0; i < base; i += 1) {
-//             let pixel = document.createElement('div')
+function createBoard(base) {
+    for (let index = 0; index < base; index += 1) {
+        let line = document.createElement('section')
 
-//             pixel.className = 'pixel'
-//             line.appendChild(pixel)
-//         }       
-//     }
-// }
+        grid.appendChild(line)
+        for (let i = 0; i < base; i += 1) {
+            let pixel = document.createElement('div')
 
-// pixels.addEventListener('change', createBoard(document.querySelector('#grid-length').value))
+            pixel.className = 'pixel'
+            line.appendChild(pixel)
+        }       
+    }
+}
+
+pixels.addEventListener('change', createBoard(document.querySelector('#grid-length').value))
 
 function pickColor (event) {
     for (let index = 0; index < colorPallet.length; index += 1) {
