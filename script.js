c4f585fa7767b444f46dab991cccaefa7c5c34f3
André Rodrigues <andre360.ars@gmail.com>
2021-12-13 22:46:02 -0300

    Requisito 12 - bonus - cria funções que trocam as cores da paleta aleatóriamente

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const pixels = document.getElementsByClassName('pixel');
 const boardSize = document.getElementById('board-size');
 const vqvButton = document.getElementById('generate-board')
 const child = grid.childElementCount
-
+let colorRgb = []
 
 function createBoard() {
     if (boardSize.value === '') {
@@ -75,3 +75,43 @@ function eraser(event) {
 for (let i = 0; i < pixels.length; i += 1){
     grid.addEventListener('dblclick', eraser);
 }
+
+function randomNumbers() {
+    let rgb = []
+    for (let i = 0; i < 3; i += 1) {
+        rgb.push(Math.floor(Math.random() * 255) + 1)
+    }
+
+    return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'  
+}
+
+function randomColors() {
+    for (let index = 0; index < 3; index += 1){
+       let color = randomNumbers();
+        
+        if (index === 0) {
+            colorRgb.push(color);
+        }
+        if (index === 1 && color !== colorRgb[0]) {
+            colorRgb.push(color)
+        } else if (index === 1) {
+            colorRgb.push(randomNumbers())
+        }
+        if (index === 2 && color !== colorRgb[0] && color !== colorRgb[1]) {
+            colorRgb.push(color)
+        } else if (index === 2) {
+            colorRgb.push(randomNumbers())
+        }
+    }
+}
+
+function palletGenerator() {
+    randomColors()
+
+    document.getElementById('color2').style.backgroundColor = colorRgb[0]
+    document.getElementById('color3').style.backgroundColor = colorRgb[1]
+    document.getElementById('color4').style.backgroundColor = colorRgb[2]
+}
+
+window.onload = palletGenerator
+
